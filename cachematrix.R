f54659cd0bd50e8aec07f60995c5018fd6bbd87d
sridhar <sridharmp@gmail.com>
2015-05-24 12:12:07 -0700

    Functions have been updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,35 @@
-## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## makeCacheMatrix: function creates a special matrix object that can cache its inverse.
+
 ## This program assumes that the matrix supplied is always invertible.
 
-makeCacheMatrix <- function(x = matrix()) {
-  inverse1 <- NULL
+makeCacheMatrix <- function(x = matrix()) 
+{
+  invrs <- NULL
   set <- function(y)   # Set the value of the matrix
-  {
+    {
     x <<- y
-    inverse1 <<- NULL
-  }
+    invrs <<- NULL
+    }
   get <- function(y) x  # Get the value of the matrix
-  setinverse <- function(inverse) inverse1 <<- inverse   #Set the value of the inverse of the matrix
-  getinverse <- function() inverse1                      #Get the value of the inverse of the matrix
+  setinverse <- function(inverse) invrs <<- inverse   #Set the value of the inverse of the matrix
+  getinverse <- function() invrs                      #Get the value of the inverse of the matrix
   list(set=set, get=get,setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## cacheSolve: This function computes the inverse of the special "matrix" returned by 
+## cacheSolve: function computes the inverse of the special matrix returned by 
 ## makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not 
-## changed), then cacheSolve should retrieve the inverse from the cache.
+## changed), then cacheSolve will retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-  inverse1 <- x$getinverse()
-  if(!is.null(inverse1)){
+cacheSolve <- function(x, ...) 
+{
+  invrs <- x$getinverse()
+  if(!is.null(invrs))
+    {
     message("Getting the Cached Data")
-    return(inverse1)
-  }
+    return(invrs)
+    }
   data <- x$get()
-  inverse1 <- solve(data)
-  x$setinverse(inverse1)
+  invrs <- solve(data)
+  x$setinverse(invrs)
 }
